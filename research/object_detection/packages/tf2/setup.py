c7e044e9f041a9191f4d90570c7608719d5f6a6c
trung78z <trungpspy@gmail.com>
2025-05-03 01:00:09 +0700

    update dependence

--- research/object_detection/packages/tf2/setup.py
+++ research/object_detection/packages/tf2/setup.py
@@ -4,49 +4,26 @@ from setuptools import find_packages
 from setuptools import setup
 
 REQUIRED_PACKAGES = [
-    'avro-python3',                    # For reading AVRO file formats
-    'apache-beam',                    # Required for TFRecord creation with Beam pipelines
-    'pillow',                         # Image handling (PIL)
-    'lxml',                           # For parsing XML annotations (e.g., Pascal VOC)
-    'matplotlib',                     # Plotting and visualizations
-    'Cython',                         # Used to compile pycocotools
-    'contextlib2',                    # Compatibility layer for contextlib
-    'tf-slim',                        # Lightweight TF model definition library
-    'six',                            # Python 2 and 3 compatibility
-    'pycocotools',                    # COCO evaluation tools
-    'lvis',                           # LVIS dataset evaluation support
-    'scipy',                          # Scientific computations, used in data pipelines
-    'pandas',                         # Data handling and manipulation
-    'tf-models-official>=2.14.0,<2.15.0'  # Official TF model implementations, match TF version
-    'tensorflow==2.14.0',             # Main deep learning framework (version 2.14)
-    'tensorflow_io==0.34.0',          # TF IO extensions, 0.34.0 compatible with TF 2.14
-    'keras==2.14.0',                  # Keras library version matching TF 2.14
-    'pyparsing==2.4.7',               # Prevents parsing issues in older matplotlib versions
-    'sacrebleu<=2.2.0',               # BLEU score evaluation (used in NLP models)
+    # Required for apache-beam with PY3
+    'avro-python3',
+    'apache-beam',
+    'pillow',
+    'lxml',
+    'matplotlib',
+    'Cython',
+    'contextlib2',
+    'tf-slim',
+    'six',
+    'pycocotools',
+    'lvis',
+    'scipy',
+    'pandas',
+    'tf-models-official >=2.5.1, <2.16.0',
+    'tensorflow_io',
+    'keras',
+    'pyparsing==2.4.7',  # TODO(b/204103388)
+    'sacrebleu<=2.2.0'  # https://github.com/mjpost/sacrebleu/issues/209
 ]
-
-
-# REQUIRED_PACKAGES = [
-#     # Required for apache-beam with PY3
-#     'avro-python3',
-#     'apache-beam',
-#     'pillow',
-#     'lxml',
-#     'matplotlib',
-#     'Cython',
-#     'contextlib2',
-#     'tf-slim',
-#     'six',
-#     'pycocotools',
-#     'lvis',
-#     'scipy',
-#     'pandas',
-#     'tf-models-official >2.10.0, <2.16.0',
-#     'tensorflow_io',
-#     'keras',
-#     'pyparsing==2.4.7',  # TODO(b/204103388)
-#     'sacrebleu<=2.2.0'  # https://github.com/mjpost/sacrebleu/issues/209
-# ]
 
 setup(
     name='object_detection',
