8cd3cc3338a9fb3fca712174f2607827420cb95a
trung78z <trungpspy@gmail.com>
2025-05-10 12:28:01 +0700

    update script

--- research/s_install.sh
+++ research/s_install.sh
@@ -12,7 +12,3 @@ cp object_detection/packages/tf2/setup.py .
 echo "Install Library...."
 pip install .
 echo "Done install library!"
-
-echo "Running test...."
-python object_detection/builders/model_builder_tf2_test.py
-echo "Run test successfully"
---
+++ research/s_test.sh
@@ -0,0 +1,3 @@
+echo "Running test...."
+python object_detection/builders/model_builder_tf2_test.py
+echo "Run test successfully"
