cd5fa58fef36ff66ce2b08c6b61c96359609c996
trung78z <trungpspy@gmail.com>
2025-05-11 21:21:13 +0700

    update opencv with c++

--- capture/setup_opencv.sh
+++ capture/setup_opencv.sh
@@ -2,7 +2,7 @@ sudo apt update
 sudo apt install gcc-12 g++-12
 sudo apt install libgtk2.0-dev pkg-config
 sudo apt install ffmpeg
-sudo apt install libcanberra-gtk-modulelibcanberra-gtk3-module
+sudo apt install libcanberra-gtk-module libcanberra-gtk3-module
 cd build
 
 cmake -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules -DWITH_CUDA=ON -DCUDA_ARCH_BIN="7.5" -DWITH_NVCUVID=OFF -DWITH_NVCUVENC=OFF -DWITH_OPENGL=ON ../opencv
